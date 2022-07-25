e4624fee75b9d89100ce0cc47b8684fda1eb03ad
Bastien BENNI <104000001+Bastou06@users.noreply.github.com>
2022-07-25 14:49:43 +0200

    Route /newuser

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/admin/newusers', [UserController::class, 'store']);
+Route::post('/newusers', [UserController::class, 'store']);
 
 //PAGE PROFIL
 //route::get('/ProfilPages', [ProfilPageController::class, 'index'])->name('profilPages.index');
