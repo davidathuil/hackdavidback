fda8a6f676c739dea70b14d1e1ec6e58b4557161
AmbivalenceR <ambivalence2806@gmail.com>
2022-07-28 01:13:57 +0200

    p

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/admin/newusers', [AuthController::class, 'register']);
-Route::post('/newusers/{id}', [UserController::class, 'store']);
 Route::post('/loginevent/{id}', [AuthController::class, 'loginevent']);
 
 //PAGE PROFIL
@@ -76,7 +75,9 @@ Route::middleware('auth:sanctum')->get('/profiles', [ProfilPageController::class
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'user'])->name('users.user');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
+// Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::post('/newusers/{id}', [UserController::class, 'store']);
+Route::post('/users', [UserController::class, 'userCreatedByAdmin']);
 Route::delete('/users/{id?}', [UserController::class, 'destroy'])->name('users.destroy.id');
 // SKILLS
 Route::get('/skills', [SkillController::class, 'index'])->name('skills.index');
